24c83aaf075ee5e99e8fe9cbd0bd37c8cbf4dab1
Navinkumar Boddu <swarmrobots@gmail.com>
2024-03-20 18:03:01 +0530

    h2 and enity setup

--- trackzill-demo/module4/src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
+++ trackzill-demo/module4/src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
@@ -1,19 +1,37 @@
 package com.pluralsight.fundamentals;
 
+import com.pluralsight.fundamentals.entity.Application;
+import com.pluralsight.fundamentals.repository.ApplicationRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-/*@SpringBootApplication - contains 3 annotations
- * @SpringBootConfiguration
- * @EnableAutoConfiguration
- * @ComponentScan
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@SpringBootApplication
 public class FundamentalsApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(FundamentalsApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(FundamentalsApplication.class, args);
-        System.out.println("Hello Pluralsight");
     }
+
+    @Bean
+    public CommandLineRunner demo(ApplicationRepository repository) {
+        return (args) -> {
+            repository.save(new Application("Trackzilla","kesha.williams","Application for tracking bugs."));
+            repository.save(new Application("Expenses","mary.jones","Application to track expense reports."));
+            repository.save(new Application("Notifications","karen.kane","Application to send alerts and notifications to users."));
+
+            for (Application application : repository.findAll()) {
+                log.info("The application is: " + application.toString());
+            }
+        };
+    }
+
 }
