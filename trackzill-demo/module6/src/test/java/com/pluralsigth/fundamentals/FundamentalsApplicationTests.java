c5ab216e82b84427d5a900bdb0efd0b2a850f591
Navinkumar Boddu <swarmrobots@gmail.com>
2024-03-21 13:15:57 +0530

    initial commit

---
+++ trackzill-demo/module6/src/test/java/com/pluralsigth/fundamentals/FundamentalsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.pluralsigth.fundamentals;
+
+import org.junit.jupiter.api.Test;
+
+public class FundamentalsApplicationTests {
+
+    @Test
+    public  void contextLoads(){
+
+    }
+}
