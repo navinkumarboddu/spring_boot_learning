e18e05e64e8242350cc77203ecba4af946c3a731
Navinkumar Boddu <swarmrobots@gmail.com>
2024-03-21 14:10:24 +0530

    initial commit

---
+++ trackzill-demo/module7/src/main/java/com/pluralsight/fundamentals/service/ApplicationService.java
@@ -0,0 +1,13 @@
+package com.pluralsight.fundamentals.service;
+
+import com.pluralsight.fundamentals.entity.Application;
+
+import java.util.List;
+
+public interface ApplicationService {
+    List<Application> listApplications();
+
+    Application findApplication(long id);
+}
+
+
---
+++ trackzill-demo/module7/src/main/java/com/pluralsight/fundamentals/service/ReleaseService.java
@@ -0,0 +1,10 @@
+package com.pluralsight.fundamentals.service;
+
+import com.pluralsight.fundamentals.entity.Release;
+
+import java.util.List;
+
+public interface ReleaseService {
+    List<Release> listReleases();
+}
+
