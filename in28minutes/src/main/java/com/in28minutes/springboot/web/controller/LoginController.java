3276ffcb43618fbf9ee3460dbbfac2b287d59558
Navinkumar Boddu <nbod@softwareag.com>
2024-04-25 23:24:20 +0530

    Closes #3 login.jsp Added

--- in28minutes/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
+++ in28minutes/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.in28minutes.springboot.web.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
     @RequestMapping("/login")
-    @ResponseBody
     public String loginMessage(){
-        return "Hello World";
+        return "login";
     }
 }
-
