b9f23aa3be9a85b82495ad93de84b5116df50162
Navinkumar Boddu <nbod@softwareag.com>
2024-04-26 23:27:00 +0530

    Resolve the login.jsp issues

--- in28minutes/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
+++ in28minutes/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.in28minutes.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
