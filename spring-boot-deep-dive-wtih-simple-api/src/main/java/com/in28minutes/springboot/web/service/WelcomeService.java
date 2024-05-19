bb3e6caecfa5c298985b12bbb4d2321b0ce18405
Navinkumar Boddu <nbod@softwareag.com>
2024-05-19 22:40:53 +0530

    Closes #32

--- spring-boot-deep-dive-wtih-simple-api/src/main/java/com/in28minutes/springboot/web/service/WelcomeService.java
+++ spring-boot-deep-dive-wtih-simple-api/src/main/java/com/in28minutes/springboot/web/service/WelcomeService.java
@@ -1,12 +1,16 @@
 package com.in28minutes.springboot.web.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WelcomeService {
 
+	@Value("${welcome.message}")
+	private String welcomMessage;
+	
 	public String retrieveWelcomeMessage() {
 		//Complex Method
-		return "Good Morning updated";
+		return welcomMessage;
 	}
 }
