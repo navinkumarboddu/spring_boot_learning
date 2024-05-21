be4a7d76ed6c5f2c83bca43719d108d3bfe29aa3
Navinkumar Boddu <nbod@softwareag.com>
2024-05-21 10:29:49 +0530

    Closes #34

--- spring-boot-deep-dive-wtih-simple-api/src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
+++ spring-boot-deep-dive-wtih-simple-api/src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
@@ -1,16 +1,20 @@
 package com.in28minutes.springboot.web.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.in28minutes.springboot.web.configuration.BasicConfiguration;
 import com.in28minutes.springboot.web.service.WelcomeService;
 
 @RestController
 public class WelcomeController {
 
-	//Auto wiring
+	// Auto wiring
 	@Autowired
 	private WelcomeService service;
 
@@ -18,4 +22,18 @@ public class WelcomeController {
 	public String welcome() {
 		return service.retrieveWelcomeMessage();
 	}
+
+	@Autowired
+	private BasicConfiguration configuration;
+
+	@RequestMapping("/dynamic-configuration")
+	public Map dynamicConfiguration() {
+		// Not the best practice to use a map to store differnt types!
+		Map map = new HashMap();
+		map.put("message", configuration.getMessage());
+		map.put("number", configuration.getNumber());
+		map.put("key", configuration.isValue());
+		return map;
+	}
+
 }
