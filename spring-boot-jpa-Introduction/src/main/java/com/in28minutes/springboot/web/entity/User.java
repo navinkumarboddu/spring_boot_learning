a397971b9e8edafe61d8b8b24089c495954a0d69
Navinkumar Boddu <swarmrobots@gmail.com>
2024-05-30 23:02:59 +0530

    updated entity and added service

--- spring-boot-jpa-Introduction/src/main/java/com/in28minutes/springboot/web/entity/User.java
+++ spring-boot-jpa-Introduction/src/main/java/com/in28minutes/springboot/web/entity/User.java
@@ -1,7 +1,14 @@
 package com.in28minutes.springboot.web.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String name;
---
+++ spring-boot-jpa-Introduction/src/main/java/com/in28minutes/springboot/web/service/UserDAOService.java
@@ -0,0 +1,21 @@
+package com.in28minutes.springboot.web.service;
+
+import com.in28minutes.springboot.web.entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
+@Transactional
+public class UserDAOService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Long saveUser(User user){
+        entityManager.persist(user);
+        return user.getId();
+    }
+}
